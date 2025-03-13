350de7fe93495c541425b800cdbd5d1199cf0c35
Giulia <giuliar1@al.insper.edu.br>
2025-03-13 19:30:40 -0300

    testeex1

--- exe1/main.c
+++ exe1/main.c
@@ -2,50 +2,54 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int BTN_PIN_R = 28;
-const int LED_PIN_R = 4;
+#define BTN_PIN_R 28
+#define LED_PIN_R 4
 
-volatile int flag_f_r = 0;
-volatile int timer = 0;
+volatile bool piscaled = false; 
+volatile bool timer_ativo = false;
 
-void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        flag_f_r = 1;
-    } else if (events == 0x8) { // rise edge
-    }
+bool timer_callback(repeating_timer_t *rt) {
+    timer_ativo = true;
+    return true;
 }
 
-bool timer_callback(repeating_timer_t *rt){
-    timer = 1;
-    return true;
+void btn_callback(uint gpio, uint32_t events) {
+    if (events & GPIO_IRQ_EDGE_FALL) {
+        piscaled = !piscaled;
+    }
 }
 
 int main() {
     stdio_init_all();
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0);
+
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
-    int timer_ms = 500; 
-    repeating_timer_t timer;
-    bool piscaled = false; 
 
-    if (!add_repeating_timer_ms(timer_ms, timer_callback, NULL, &timer)){
-        printf("Erro para adicionar o timer\n");
+    gpio_set_irq_enabled_with_callback(
+        BTN_PIN_R,
+        GPIO_IRQ_EDGE_FALL,
+        true,
+        &btn_callback
+    );
+
+    repeating_timer_t timer;
+    if (!add_repeating_timer_ms(500, timer_callback, NULL, &timer)) {
+        printf("Falha ao adicionar o timer\n");
     }
+
     while (true) {
-        if (flag_f_r) { 
-            flag_f_r = 0; 
-            piscaled = !piscaled; 
-            if (!piscaled) {
-                gpio_put(LED_PIN_R, 0);
+        if (piscaled) {
+            if (timer_ativo) {
+                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+                timer_ativo = false;
             }
+        } else {
+            gpio_put(LED_PIN_R, 0);
         }
-        if (piscaled && timer) {
-            timer = 0;
-            gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R)); 
-        }
+        sleep_ms(10); 
     }
 }
